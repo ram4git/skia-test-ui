8d767de1a269c2d7eedc7edd98849935b47d209d
Ram <ram@sonet.io>
2022-05-17 12:34:09 -0500

    just the text

--- pages/index.js
+++ pages/index.js
@@ -14,9 +14,9 @@ export default function Home() {
 
           <main className='h-screen w-screen max-w-4xl mx-auto grid justify-center items-center'>
               <div className="grid grid-flow-row-dense ">
-                <div className=' grid grid-flow-col-dense justify-center'>
+                {/* <div className=' grid grid-flow-col-dense justify-center'>
                     <div className=" h-36 w-36 rounded-full border-8 border-green-900"></div>
-                </div>
+                </div> */}
                 <h1 className="text-center text-2xl text-gray-900 py-6">The quick brown fox <span className='bold text-xl text-blue-900'>jumps</span> over the <span className=' font-thin font-mono text-green-900'>lazy</span> dog</h1>
             </div>
         </main>
