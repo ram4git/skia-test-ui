3292cb5db38fc891576a03012019acabe1ed1086
Ram <ram@sonet.io>
2022-05-31 17:05:33 -0500

    added auth page with post message

---
+++ pages/auth.js
@@ -0,0 +1,16 @@
+import { useEffect } from "react"
+
+
+export default function Auth() {
+
+    useEffect(() => {
+        console.log("Auth.js: useEffect()")
+        window.postMessage({ type: 'SKIA_INSPECTOR_SHOW', creds: { username: 'ram-user', password: 'ram-password' } }, '*')
+    }, []); 
+
+
+    return (<form className="shadow-lg h-48 my-24 p-8 rounded-md bg-blue-500 grid grid-flow-row max-w-lg mx-auto">
+        <input className="h-8 rounded-full text-lg pl-8 text-green-500" name="email" type="text" placeholder="username" />
+        <input className="h-8 rounded-full text-lg pl-8 text-green-500" name="password" type="password" placeholder="password" />
+  </form>)
+}
