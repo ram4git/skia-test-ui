ed3bbbaf1cf45a591c17112fd00e67cd6d4ef75a
Ram <ram@sonet.io>
2022-06-09 10:58:57 -0500

    added mono page

--- pages/f/mono.js
+++ pages/f/mono.js
@@ -1,7 +1,3 @@
-import styles from '../styles/Home.module.css'
-import Head from 'next/head'
-import Image from 'next/image'
-
 export default function Home() {
-  return (<p className="font-mono text-lg">Its a wonderful day to inspect skia</p>)
+  return (<p className="font-mono text-xl text-blue-700">Its a wonderful day to inspect skia</p>)
 }
