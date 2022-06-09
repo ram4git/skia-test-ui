e7ebbf8c995cdc9612b6106ac4a5f418b09f4415
Ram <ram@sonet.io>
2022-06-09 10:55:50 -0500

    added page with a different font

---
+++ pages/f/mono.js
@@ -0,0 +1,7 @@
+import styles from '../styles/Home.module.css'
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (<p className="font-mono text-lg">Its a wonderful day to inspect skia</p>)
+}
