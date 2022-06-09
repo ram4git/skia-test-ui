4479f6a658bf62e24f22f7919b182b66c75d0ae4
Ram <ram@sonet.io>
2022-06-09 16:39:05 -0500

    larger mono

--- pages/auth.js
+++ pages/auth.js
@@ -5,12 +5,15 @@ export default function Auth() {
 
     useEffect(() => {
         console.log("Auth.js: useEffect()")
-        window.postMessage({ type: 'SKIA_INSPECTOR_SHOW', creds: { username: 'ram-user', password: 'ram-password' } }, '*')
-    }, []); 
+        // window.postMessage({ type: 'SKIA_INSPECTOR_SHOW', creds: { username: 'ram-user', password: 'ram-password' } }, '*')
+        window.postMessage({ type: 'SONET_AUTH_INIT', creds: { username: 'sam-user', password: 'sam-password' } }, '*');
+    }, []);
 
 
-    return (<form className="shadow-lg h-48 my-24 p-8 rounded-md bg-blue-500 grid grid-flow-row max-w-lg mx-auto">
-        <input className="h-8 rounded-full text-lg pl-8 text-green-500" name="email" type="text" placeholder="username" />
-        <input className="h-8 rounded-full text-lg pl-8 text-green-500" name="password" type="password" placeholder="password" />
-  </form>)
+    return (
+        <form className="shadow-lg h-48 my-24 p-8 rounded-md bg-blue-500 grid grid-flow-row max-w-lg mx-auto">
+            <input className="h-8 rounded-full text-lg pl-8 text-green-500" name="email" type="text" placeholder="username" />
+            <input className="h-8 rounded-full text-lg pl-8 text-green-500" name="password" type="password" placeholder="password" />
+        </form>
+    )
 }
--- pages/f/bigmono.js
+++ pages/f/bigmono.js
@@ -1,3 +1,4 @@
 export default function Home() {
   return (<p className="font-mono text-4xl text-blue-700">Its a wonderful day to inspect skia</p>)
 }
+
