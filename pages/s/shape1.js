763a3972b2cc226891c3e24b2299291413b51147
Ram <ram@sonet.io>
2022-06-10 10:40:08 -0500

    added shapes

---
+++ pages/s/shape1.js
@@ -0,0 +1,32 @@
+import Head from 'next/head';
+export default function icon1() {
+	return (
+		<>
+			<Head>
+				<title>Sonet</title>
+				<meta name="description" content="sonet demo UI" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<main className="h-screen w-screen max-w-4xl mx-auto grid justify-center items-center">
+				<div className="h-48 w-48">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						className="h-full w-full text-green-500"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+						strokeWidth="2"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"
+						/>
+						<path strokeLinecap="round" strokeLinejoin="round" d="M15 13a3 3 0 11-6 0 3 3 0 016 0z" />
+					</svg>
+				</div>
+			</main>
+		</>
+	);
+}
---
+++ pages/s/shape2.js
@@ -0,0 +1,40 @@
+import Head from 'next/head';
+export default function icon1() {
+	return (
+		<>
+			<Head>
+				<title>Sonet</title>
+				<meta name="description" content="sonet demo UI" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<main className="h-screen w-screen max-w-4xl mx-auto grid justify-center items-center">
+				<div className="flex flex-row">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						className="h-24 w-24 text-green-500"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+						strokeWidth="2"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"
+						/>
+						<path strokeLinecap="round" strokeLinejoin="round" d="M15 13a3 3 0 11-6 0 3 3 0 016 0z" />
+                    </svg>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-24 w-24 text-red-500"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                    >
+                        <path fillRule="evenodd" d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V4a2 2 0 00-2-2H6zm1 2a1 1 0 000 2h6a1 1 0 100-2H7zm6 7a1 1 0 011 1v3a1 1 0 11-2 0v-3a1 1 0 011-1zm-3 3a1 1 0 100 2h.01a1 1 0 100-2H10zm-4 1a1 1 0 011-1h.01a1 1 0 110 2H7a1 1 0 01-1-1zm1-4a1 1 0 100 2h.01a1 1 0 100-2H7zm2 1a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1zm4-4a1 1 0 100 2h.01a1 1 0 100-2H13zM9 9a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1zM7 8a1 1 0 000 2h.01a1 1 0 000-2H7z" clipRule="evenodd" />
+                    </svg>
+				</div>
+			</main>
+		</>
+	);
+}
