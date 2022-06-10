2d357c925479bd1115fd62dbf18bc966f80b87d0
Ram <ram@sonet.io>
2022-06-10 13:14:35 -0500

    added okta link

--- pages/index.js
+++ pages/index.js
@@ -4,5 +4,12 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  return (<p>Its a wonderful day to inspect skia</p>)
+    return (
+    <div>
+        <div>
+            <a href="https://sonet.okta.com">Sonet OKTA</a>
+            <a href="https://jira.com">JIRA</a>
+            </div>
+            <p>Its a wonderful day to inspect skia</p>
+  </div>)
 }
