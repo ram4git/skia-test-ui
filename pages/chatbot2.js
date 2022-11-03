bd8dbc1386e3980ee63d14e2ace66cebb55a91b1
Ram <ram@sonet.io>
2022-11-03 10:41:24 -0500

    hello page without chatbot

--- pages/chatbot2.js
+++ pages/chatbot2.js
@@ -7,16 +7,6 @@ import styles from '../styles/Home.module.css'
 
 export default function Img1() {
 
-    useEffect(() => {
-        window.__be = window.__be || {};
-        window.__be.id = "6362751136e08c0007252a6f";
-        (function () {
-            var be = document.createElement('script'); be.type = 'text/javascript'; be.async = true;
-            be.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.chatbot.com/widget/plugin.js';
-            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(be, s);
-        })();
-    }, []);
-
     return (
         <div className={styles.container}>
             <Head>
