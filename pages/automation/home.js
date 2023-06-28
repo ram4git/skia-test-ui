7c76512a1d06b3ac6d6be3e3974aa5d31639faf4
Ram <ram@sonet.io>
2023-06-28 11:21:22 -0500

    added next link instead of a

--- pages/automation/home.js
+++ pages/automation/home.js
@@ -1,5 +1,5 @@
-import { useEffect } from "react";
 import { useState } from "react";
+import Link from 'next/link'
 
 const Home = () => {
   const [val, setValue] = useState("bg-red-500");
@@ -17,12 +17,14 @@ const Home = () => {
           <p className="font-bold text-center text-sm text-gray-900 pt-4">{`You searched for ${val}`}</p>
         ) : null}
       </div>
-      <a
-        href="/automation/landing"
-        className="absolute top-2 left-2 py-2 px-6 bg-blue-500 text-white rounded-sm shadow-sm"
-      >
-        CLICK ME TO REDIRECT
-      </a>
+      <div className="absolute top-2 left-2 py-2 px-6 bg-blue-500 text-white rounded-sm shadow-sm">
+        <Link
+            href="/automation/landing"
+            className=" bg-blue-500 text-white rounded-sm shadow-sm"
+        >
+            CLICK ME TO REDIRECT
+        </Link>
+      </div>
     </div>
   );
 };
