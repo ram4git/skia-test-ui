bec0d0b0c9c03e10848dd7dc06fe92747260087a
PiyushF27 <piyush.patil@finasyst.in>
2023-07-06 14:37:05 +0530

    added debug

--- pages/automation/redirect.js
+++ pages/automation/redirect.js
@@ -7,10 +7,15 @@ const Redirect = () => {
 
   const [val, setValue] = useState("bg-red-500");
 
+  const onRedirectHandle = () => {
+    console.log('hit:::::::::::::::::::')
+    router.push('/automation/landing')
+  }
+
   return (
     <div className="h-screen w-screen grid place-items-center relative">
       <div className="absolute top-0 left-0 py-1 px-1 bg-blue-500 text-white rounded-sm shadow-sm">
-        <button type="button" onClick={() => router.push('/automation/landing')}>
+        <button type="button" onClick={onRedirectHandle}>
         CLICK ME TO REDIRECT
     </button>
       </div>
